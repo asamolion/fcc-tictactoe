d0eeed93d07a25f50683f2e74f042e69f54e0736
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2017-11-12 23:39:08 +0500

    Improve: implement slide functions

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var pug = require("gulp-pug");
 var browserSync = require("browser-sync").create();
+var babel = require('gulp-babel');
 var plumber = require("gulp-plumber");
 
 gulp.task("sass", function() {
@@ -34,6 +35,9 @@ gulp.task("script", function() {
   return gulp
     .src("src/**/*.js")
     .pipe(plumber())
+    .pipe(babel({
+      presets: ['env']
+    }))
     .pipe(gulp.dest("build"));
 });
 
@@ -48,6 +52,6 @@ gulp.task("browserSync", function() {
 gulp.task("watch", ["browserSync", "sass", "markup", "script"], function() {
   gulp.watch("src/**/*.scss", ["sass", browserSync.reload]);
   gulp.watch("src/**/*.pug", ["markup", browserSync.reload]);
-  gulp.watch("src/**/*.js", browserSync.reload);
+  gulp.watch("src/**/*.js", ["script", browserSync.reload]);
   // Other watchers
 });
