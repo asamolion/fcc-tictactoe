3c59593c3c7de32876f1c51afc6370b877e64904
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2017-12-10 16:07:03 +0500

    Improve: implement player ad AI choice

--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,58 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var pug = require("gulp-pug");
 var browserSync = require("browser-sync").create();
-var babel = require('gulp-babel');
+var babel = require("gulp-babel");
 var plumber = require("gulp-plumber");
 
 gulp.task("sass", function() {
-  return gulp
-    .src("src/**/*.scss") // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest("build"))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
+    return gulp
+        .src("src/**/*.scss") // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("build"))
+        .pipe(
+            browserSync.reload({
+                stream: true
+            })
+        );
 });
 
 gulp.task("markup", function() {
-  return gulp
-    .src("src/**/*.pug")
-    .pipe(plumber())
-    .pipe(pug())
-    .pipe(gulp.dest("build"))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
+    return gulp
+        .src("src/**/*.pug")
+        .pipe(plumber())
+        .pipe(pug())
+        .pipe(gulp.dest("build"))
+        .pipe(
+            browserSync.reload({
+                stream: true
+            })
+        );
 });
 
 gulp.task("script", function() {
-  return gulp
-    .src("src/**/*.js")
-    .pipe(plumber())
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(gulp.dest("build"));
+    return gulp
+        .src("src/**/*.js")
+        .pipe(plumber())
+        .pipe(
+            babel({
+                presets: ["env"]
+            })
+        )
+        .pipe(gulp.dest("build"));
 });
 
 gulp.task("browserSync", function() {
-  browserSync.init({
-    server: {
-      baseDir: "build"
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "build"
+        }
+    });
 });
 
 gulp.task("watch", ["browserSync", "sass", "markup", "script"], function() {
-  gulp.watch("src/**/*.scss", ["sass", browserSync.reload]);
-  gulp.watch("src/**/*.pug", ["markup", browserSync.reload]);
-  gulp.watch("src/**/*.js", ["script", browserSync.reload]);
-  // Other watchers
+    gulp.watch("src/**/*.scss", ["sass", browserSync.reload]);
+    gulp.watch("src/**/*.pug", ["markup", browserSync.reload]);
+    gulp.watch("src/**/*.js", ["script", browserSync.reload]);
+    // Other watchers
 });
